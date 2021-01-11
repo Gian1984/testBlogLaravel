46974a7d7315a39f73aaaf782fb427339d3f5976
“Gian1984” <gl.tiengo@gmail.com>
2021-01-11 16:47:46 +0100

    working with posts

--- test-blog-laravell/app/Http/Controllers/PostController.php
+++ test-blog-laravell/app/Http/Controllers/PostController.php
@@ -13,6 +13,15 @@ class PostController extends Controller
 
     public function store(Request $request)
     {
-        dd('ok');
+        $this->validate($request, [
+            'body'=>'required'
+        ]);
+
+        Post::create([
+            
+            'user_id'=>auth()->id(),
+            'body'=>$request->body
+
+        ]);
     }
 }
